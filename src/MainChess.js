5cc642b07d59ac844e07a14dee2271b494380fae
Ilya <mac@MacBookPro.local>
2022-04-28 14:53:21 +0300

    add ids all tags

--- src/MainChess.js
+++ src/MainChess.js
@@ -45,6 +45,10 @@ export default () => {
     setPlayGame(0);
   };
 
+  const activeMove = ({ id }) => {
+    console.log('id', id);
+  };
+
   return (
     <div className="game">
       <div className="game-users">
@@ -77,139 +81,139 @@ export default () => {
         </div>
 
         <div className="Dashboard">
-          <div className="cell">
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="A8">
             <img src={Tower} alt="tower" />
           </div>
-          <div className="cell black">
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="B8">
             <img src={Horse} alt="horse" />
           </div>
-          <div className="cell">
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="C8">
             <img src={Elephant} alt="elephant" />
           </div>
-          <div className="cell black">
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="D8">
             <img src={Lady} alt="lady" />
           </div>
-          <div className="cell">
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="E8">
             <img src={King} alt="king" />
           </div>
-          <div className="cell black">
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="F8">
             <img src={Elephant} alt="elephant" />
           </div>
-          <div className="cell">
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="G8">
             <img src={Horse} alt="horse" />
           </div>
-          <div className="cell black">
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="H8">
             <img src={Tower} alt="tower" />
           </div>
 
-          <div className="cell black">
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="A7">
             <img src={Pawn} alt="pawn" />
           </div>
-          <div className="cell">
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="B7">
             <img src={Pawn} alt="pawn" />
           </div>
-          <div className="cell black">
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="C7">
             <img src={Pawn} alt="pawn" />
           </div>
-          <div className="cell">
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="D7">
             <img src={Pawn} alt="pawn" />
           </div>
-          <div className="cell black">
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="E7">
             <img src={Pawn} alt="pawn" />
           </div>
-          <div className="cell">
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="F7">
             <img src={Pawn} alt="pawn" />
           </div>
-          <div className="cell black">
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="G7">
             <img src={Pawn} alt="pawn" />
           </div>
-          <div className="cell">
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="H7">
             <img src={Pawn} alt="tower" />
           </div>
 
-          <div className="cell"></div>
-          <div className="cell black"></div>
-          <div className="cell"></div>
-          <div className="cell black"></div>
-          <div className="cell"></div>
-          <div className="cell black"></div>
-          <div className="cell"></div>
-          <div className="cell black"></div>
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="A6" />
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="B6" />
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="C6" />
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="D6" />
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="E6" />
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="F6" />
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="G6" />
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="H6" />
 
-          <div className="cell black"></div>
-          <div className="cell"></div>
-          <div className="cell black"></div>
-          <div className="cell"></div>
-          <div className="cell black"></div>
-          <div className="cell"></div>
-          <div className="cell black"></div>
-          <div className="cell"></div>
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="A5" />
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="B5" />
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="C5" />
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="D5" />
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="E5" />
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="F5" />
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="G5" />
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="H5" />
 
-          <div className="cell"></div>
-          <div className="cell black"></div>
-          <div className="cell"></div>
-          <div className="cell black"></div>
-          <div className="cell"></div>
-          <div className="cell black"></div>
-          <div className="cell"></div>
-          <div className="cell black"></div>
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="A4" />
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="B4" />
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="C4" />
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="D4" />
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="E4" />
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="F4" />
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="G4" />
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="H4" />
 
-          <div className="cell black"></div>
-          <div className="cell"></div>
-          <div className="cell black"></div>
-          <div className="cell"></div>
-          <div className="cell black"></div>
-          <div className="cell"></div>
-          <div className="cell black"></div>
-          <div className="cell"></div>
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="A3" />
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="B3" />
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="C3" />
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="D3" />
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="E3" />
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="F3" />
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="G3" />
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="H3" />
 
-          <div className="cell">
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="A2">
             <img src={PawnMain} alt="pawnMain" />
           </div>
-          <div className="cell black">
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="B2">
             <img src={PawnMain} alt="pawnMain" />
           </div>
-          <div className="cell">
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="C2">
             <img src={PawnMain} alt="pawnMain" />
           </div>
-          <div className="cell black">
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="D2">
             <img src={PawnMain} alt="pawnMain" />
           </div>
-          <div className="cell">
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="E2">
             <img src={PawnMain} alt="pawnMain" />
           </div>
-          <div className="cell black">
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="F2">
             <img src={PawnMain} alt="pawnMain" />
           </div>
-          <div className="cell">
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="G2">
             <img src={PawnMain} alt="pawnMain" />
           </div>
-          <div className="cell black">
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="H2">
             <img src={PawnMain} alt="pawnMain" />
           </div>
 
-          <div className="cell black">
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="A1">
             <img src={TowerMain} alt="tower" />
           </div>
-          <div className="cell">
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="B1">
             <img src={HorseMain} alt="horseMain" />
           </div>
-          <div className="cell black">
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="C1">
             <img src={ElephantMain} alt="elephantMain" />
           </div>
-          <div className="cell">
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="D1">
             <img src={LadyMain} alt="ladyMain" />
           </div>
-          <div className="cell black">
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="E1">
             <img src={KingMain} alt="kingMain" />
           </div>
-          <div className="cell">
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="F1">
             <img src={ElephantMain} alt="elephantMain" />
           </div>
-          <div className="cell black">
+          <div className="cell black" onClick={(e) => activeMove(e.currentTarget)} id="G1">
             <img src={HorseMain} alt="horseMain" />
           </div>
-          <div className="cell">
+          <div className="cell" onClick={(e) => activeMove(e.currentTarget)} id="H1">
             <img src={TowerMain} alt="towerMain" />
           </div>
         </div>
